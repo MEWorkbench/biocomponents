55f84e43b21c5c70b5df91428c4863f601504a2c
Paulo Maia <pmaia@silicolife.com>
2016-07-15 18:05:59 +0100

    [test] - JSBMLTest

--- src/test/java/pt/uminho/ceb/biosystems/mew/biocomponents/io/JSBMLTest.java
+++ src/test/java/pt/uminho/ceb/biosystems/mew/biocomponents/io/JSBMLTest.java
@@ -6,14 +6,24 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.Container;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.components.MetaboliteCI;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.components.ReactionCI;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.components.StoichiometryValueCI;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.io.readers.ErrorsException;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.io.readers.JSBMLReader;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.io.writers.JSBMLWriter;
 import pt.uminho.ceb.biosystems.mew.biocomponents.validation.io.JSBMLValidationException;
 import pt.uminho.ceb.biosystems.mew.biocomponents.validation.io.JSBMLValidator;
 import pt.uminho.ceb.biosystems.mew.biocomponents.validation.io.jsbml.validators.JSBMLValidatorException;
@@ -71,6 +81,53 @@ public class JSBMLTest {
 //	@Test
 	public void correctIdentation() throws FileNotFoundException, ParserConfigurationException, SAXException, IOException, TransformerException{
 		JSBMLValidator.correctIdentation(new FileInputStream(getFile("model_with_problems/iJP815.xml")), new FileOutputStream(getFile("solved_problem_models/iJP815.xml")));
+	}
+	
+//	public static void main(String[] args) throws IOException, XMLStreamException, ErrorsException, ParserConfigurationException, SAXException, JSBMLValidationException {
+//		JSBMLReader reader = new JSBMLReader("/home/pmaia/ownCloud/documents/INVISTA/INVISTA_20160118_landscape_tests/model/iJO1366_AHPT.xml", "ecoli",false);
+//		Container container = new Container(reader);
+//	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		String baseDir = "/home/pmaia/ownCloud/documents/INVISTA/INVISTA_20160118_landscape_tests/";
+		
+		String conf = baseDir + "configurations/conf_20160118_landscape_test1.conf";
+		
+		JSBMLReader reader = new JSBMLReader("/home/pmaia/ownCloud/documents/INVISTA/INVISTA_20160118_landscape_tests/model/iJO1366_GLUDy_AHPT_gapA.xml", "ecoli",false);
+		Container container = new Container(reader);
+		container.setBiomassId("R_Ec_biomass_iJO1366_core_53p95M");
+		
+//		OptimizationConfiguration config = new OptimizationConfiguration(conf);
+//		config.setCurrentState(0);
+//		Container container = config.getContainer();
+//		container.setBiomassId(config.getModelBiomass());
+		
+//		ContainerEnv env = new ContainerEnv(container);
+		int numFake = 3000;
+		
+		MetaboliteCI metFake1 = new MetaboliteCI("M_metFake1", "metaboliteFake1");
+		MetaboliteCI metFake2 = new MetaboliteCI("M_metFake2", "metaboliteFake2");
+		
+		container.getMetabolites().put("M_metFake1", metFake1);
+		container.getMetabolites().put("M_metFake2", metFake2);
+		
+		for (int i = 1; i <= numFake; i++) {
+			if(i % 100 == 0) System.out.print(i+",");
+			Map<String, StoichiometryValueCI> reacMap = new HashMap<>();
+			reacMap.put("M_metFake1", new StoichiometryValueCI("M_metFake1", 1.0, "c"));
+			Map<String, StoichiometryValueCI> prodMap = new HashMap<>();
+			prodMap.put("M_metFake2", new StoichiometryValueCI("M_metFake2", 1.0, "c"));
+			
+			ReactionCI reac = new ReactionCI("R_reacFake"+i, "reaction fake"+i, false, reacMap, prodMap);
+			container.getReactions().put("R_reacFake"+i, reac);
+		}
+		container.verifyDepBetweenClass(false);
+		
+		JSBMLWriter writer = new JSBMLWriter("/home/pmaia/ownCloud/documents/INVISTA/INVISTA_20160118_landscape_tests/model/iJO1366_GLUDy_AHPT_gapA_tuned.xml", container);
+		writer.writeToFile();
+		
+		System.out.println("...Done");
 	}
 
 }
